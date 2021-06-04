ffd4324e5b400fdc1601e0509fc6c122ace81a21
zuran21 <81603223+zuran21@users.noreply.github.com>
2021-06-04 18:10:17 -0500

    arreglo
    arreglado

--- graficas1.py
+++ graficas1.py
@@ -2,7 +2,7 @@ import matplotlib as plt
 import numpy as np
 import csv
 
-with open('licenciamiento.03.csv', encoding='utf-8') as f:
-    reader = csv.reader(f)
+with open('csv/Licenciamiento_Institucional_6.csv', encoding='utf-8') as f:
+    reader = csv.reader(f, delimiter="|")
     for row in reader:
-        print("CODIGO_ENTIDAD: {0}, NOMBRE: {1}, TIPO_GESTION: {2}, ESTADO_LICENCIAMIENTO: {3}".format(row[0], row[1], row[2], row[3]))
+        print("CODIGO: {0}, NOMBRE: {1}, TIPO_GESTION: {2}, ESTADO_LICENCIAMIENTO: {3}".format(row[0], row[1], row[2], row[3]))
