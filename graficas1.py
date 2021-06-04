de30aaf4e98917e3fa2626b5606398c425f113be
zuran21 <81603223+zuran21@users.noreply.github.com>
2021-06-04 17:01:29 -0500

    wasdas 2

--- graficas1.py
+++ graficas1.py
@@ -2,7 +2,7 @@ import matplotlib as plt
 import numpy as np
 import csv
 
-with open('licenciamiento.csv', encoding='uft-8') as f:
+with open('licenciamiento2.0.csv', encoding='utf-8') as f:
     reader = csv.reader(f)
     for row in reader:
         print("CODIGO_ENTIDAD: {0}, NOMBRE: {1}, TIPO_GESTION: {2}, ESTADO_LICENCIAMIENTO: {3}".format(row[0], row[1], row[2], row[3]))
